621a6a9ae85524d24d3bc7aa87975b8a78944f42
Jill Parekh <jillpwork@gmail.com>
2019-08-19 22:34:35 +0530

    Adding custom JS file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,3 +4,15 @@ $('.scrollcustom').click(function() {
       scrollTop: $(sectionTo).offset().top
     }, 1000);
 });
+
+
+$('.scrollcustomSlow').click(function() {
+    var sectionTo = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(sectionTo).offset().top
+    }, 2500);
+});
+
+$(window).on('hashchange', function(e){
+    history.replaceState ("", document.title, e.originalEvent.oldURL);
+});
